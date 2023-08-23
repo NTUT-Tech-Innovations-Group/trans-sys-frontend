a934a0a69c37025eced0b01ce37e72a9d38fea48
LI-YONG-QI <a2369028@gmail.com>
2023-08-24 00:26:36 +0800

    init commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import { StyleSheet, Image, View, SafeAreaView, Text } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState<string>("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.image} source={require("./assets/home.jpg")} />
+      <LinearGradient
+        style={styles.mask}
+        locations={[0.8, 0.4]}
+        colors={["white", "transparent"]}
+      >
+        <Text>Hello</Text>
+      </LinearGradient>
+      {/* <View style={styles.fadeContainer}>
+        <Text>Hello</Text>
+      </View> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  image: {
+    width: 435,
+    height: 653,
+    zIndex: 0,
+  },
+  header: {
+    top: -35,
+    fontSize: 16,
+    letterSpacing: 5,
+  },
+  mask: {
+    position: "absolute",
+    zIndex: 1,
+    width: "100%",
+    height: 620,
+    top: 224,
+  },
+  fadeContainer: {
+    zIndex: 10,
+    backgroundColor: "white",
+    shadowColor: "grey",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 1,
+    shadowRadius: 40,
+    width: "100%",
+    height: 100,
   },
 });
